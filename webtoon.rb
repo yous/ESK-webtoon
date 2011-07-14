2589ce4b837d8067e4d769207f60dc8630ed9460
yous <chacha@kaist.ac.kr>
2011-07-14 04:45:38 -0700

    정규식으로 제목 검사

--- webtoon.rb
+++ webtoon.rb
@@ -4,4 +4,8 @@ require 'rubygems'
 require 'mechanize'
 
 a = Mechanize.new.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
-puts a.search('//td[@class="title"]/a')[0].inner_html
+if a.search('//td[@class="title"]/a')[0].inner_html =~ /\(\d+\)/
+  puts "Time for sleep!"
+else
+  puts "Webtoon Time!"
+end
