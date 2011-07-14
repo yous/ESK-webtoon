36aee1d133a667b5425c758f3a98f3ea92d69e2c
yous <chacha@kaist.ac.kr>
2011-07-14 03:41:18 -0700

    최근 한 화면의 모든 제목 출력

--- webtoon.rb
+++ webtoon.rb
@@ -5,4 +5,4 @@ require 'mechanize'
 
 a = Mechanize.new
 p = a.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
-puts p.search('//td[@class="title"]').inner_html
+puts p.search('//td[@class="title"]/a').inner_html
