67229ace588253e19c2f26fc47c7e2581ac0409d
yous <chacha@kaist.ac.kr>
2011-07-14 03:45:00 -0700

    최근 첫 제목 출력

--- webtoon.rb
+++ webtoon.rb
@@ -3,6 +3,5 @@
 require 'rubygems'
 require 'mechanize'
 
-a = Mechanize.new
-p = a.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
-puts p.search('//td[@class="title"]/a').inner_html
+a = Mechanize.new.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
+puts a.search('//td[@class="title"]/a')[0].inner_html
