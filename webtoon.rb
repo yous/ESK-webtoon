f3c423235574b06d62f7c5bb909bd1e16a3faf63
Minwoo Lee <ermaker@gmail.com>
2011-07-28 15:18:51 +0900

    Modify the regular expression

--- webtoon.rb
+++ webtoon.rb
@@ -5,7 +5,7 @@ require 'mechanize'
 
 def new_webtoon?
   a = Mechanize.new.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
-  a.search('//td[@class="title"]/a')[0].text =~ /\(\d+\)\s*/
+  a.search('//td[@class="title"]/a')[0].text =~ /\(\d+\)\s*\z/
 end
 
 puts new_webtoon? ? "Time for sleep!" : "Webtoon Time!"
