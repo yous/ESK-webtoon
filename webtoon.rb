d97dd55b3c14164a9dbfe15b01ede71bbc5d190a
Minwoo Lee <ermaker@gmail.com>
2011-07-28 15:13:32 +0900

    Modify if statement

--- webtoon.rb
+++ webtoon.rb
@@ -4,4 +4,4 @@ require 'rubygems'
 require 'mechanize'
 
 a = Mechanize.new.get 'http://comic.naver.com/webtoon/list.nhn?titleId=15568&weekday=thu'
-if a.search('//td[@class="title"]/a')[0].inner_html =~ /\(\d+\)\s*/ then puts "Time for sleep!" else puts "Webtoon Time!" end
+puts a.search('//td[@class="title"]/a')[0].text =~ /\(\d+\)\s*/ ? "Time for sleep!" : "Webtoon Time!"
